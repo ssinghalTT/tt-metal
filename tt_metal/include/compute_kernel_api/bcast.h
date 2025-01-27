948b9ed712cac7d8dd7f57743342f42c06fc778f
Anil Mahmud <amahmud@tenstorrent.com>
2025-01-26 22:40:59 -0500

    Add unary bcast api

--- tt_metal/include/compute_kernel_api/bcast.h
+++ tt_metal/include/compute_kernel_api/bcast.h
@@ -9,6 +9,7 @@
 #include "llk_math_binary_api.h"
 #include "llk_math_matmul_api.h"
 #include "llk_math_common.h"
+#include "llk_math_unary_datacopy_api.h"
 #endif
 #ifdef TRISC_UNPACK
 #include "llk_unpack_AB_api.h"
@@ -20,6 +21,39 @@
 #endif
 
 namespace ckernel {
+
+template <BroadcastType bcast_type>
+ALWI void unary_bcast_init(uint32_t icb, uint32_t ocb) {
+    // Will configure A & B in similar way
+    UNPACK((llk_unpack_A_hw_configure_disaggregated<DST_ACCUM_MODE>(icb)));
+    UNPACK((llk_unpack_A_init<bcast_type, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(
+        false, false /*transpose within 16x16 face*/, icb)));
+    if (bcast_type == BroadcastType::NONE) {
+        MATH((llk_math_eltwise_unary_datacopy_init<A2D, bcast_type, DST_ACCUM_MODE>(
+            false /*transpose of faces*/, false /*transpose within 16x16 face*/, icb)));
+    } else {
+        MATH((llk_math_eltwise_unary_datacopy_init<B2D, bcast_type, DST_ACCUM_MODE>(
+            false /*transpose of faces*/, false /*transpose within 16x16 face*/, icb)));
+    }
+    MATH((llk_math_pack_sync_init<DST_ACCUM_MODE>()));
+    MATH((llk_math_hw_configure_disaggregated(icb, icb)));
+
+    PACK((llk_pack_hw_configure_disaggregated<false, DST_ACCUM_MODE>(ocb)));
+    PACK((llk_pack_init<false>(ocb)));
+    PACK((llk_pack_dest_init<false, DST_ACCUM_MODE>()));
+}
+
+template <BroadcastType bcast_type>
+ALWI void unary_bcast(uint32_t icb, uint32_t in_tile_index, uint32_t dst_tile_index) {
+    UNPACK((llk_unpack_A<bcast_type, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(icb, in_tile_index)));
+    if (bcast_type == BroadcastType::NONE) {
+        MATH((llk_math_eltwise_unary_datacopy<A2D, BroadcastType::NONE, DST_ACCUM_MODE, UnpackToDestEn>(
+            dst_tile_index, icb)));
+    } else {
+        MATH((llk_math_eltwise_unary_datacopy<B2D, BroadcastType::NONE, DST_ACCUM_MODE, UnpackToDestEn>(
+            dst_tile_index, icb)));
+    }
+}
 
 /**
  * Shorthand template instantiation of sub_tiles_bcast.
